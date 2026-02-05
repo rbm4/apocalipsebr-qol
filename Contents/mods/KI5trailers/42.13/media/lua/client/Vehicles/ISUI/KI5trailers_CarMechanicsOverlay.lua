882e1d83140b515ef5a0b8c129379bc982c41deb
Ricardo <ricardo.malafaia1994@gmail.com>
2026-02-04 22:05:35 -0300

    trailers & sprinters

---
+++ Contents/mods/KI5trailers/42.13/media/lua/client/Vehicles/ISUI/KI5trailers_CarMechanicsOverlay.lua
@@ -0,0 +1,95 @@
+require "DAMN_MechOverlay";
+--
+--##########TrailerKI5utilityLarge##########
+--
+DAMN.MechOverlay:addParts({
+    ["Base.TrailerKI5utilityLarge"] = "TrailerKI5utilityLarge_",
+}, {
+    SuspensionFrontLeft = {img="suspension_front_left", x=13,y=188,x2=55,y2=225},
+    SuspensionFrontRight = {img="suspension_front_right", x=228,y=188,x2=270,y2=225},
+    SuspensionRearLeft = {img="suspension_rear_left", x=13,y=286,x2=55,y2=324},
+    SuspensionRearRight = {img="suspension_rear_right", x=228,y=286,x2=270,y2=324},
+    --
+    TireFrontLeft = {img="wheel_front_left", x=13,y=225,x2=55,y2=263},
+    TireFrontRight = {img="wheel_front_right", x=228,y=225,x2=270,y2=263},
+    TireRearLeft = {img="wheel_rear_left", x=13,y=324,x2=55,y2=361},
+    TireRearRight = {img="wheel_rear_right", x=228,y=324,x2=270,y2=361},
+    --
+    TrunkDoor = {img="trunk", x=94,y=360,x2=187,y2=364},
+}, 10, 0);
+--
+--##########TrailerKI5utilityMedium##########
+--
+DAMN.MechOverlay:addParts({
+    ["Base.TrailerKI5utilityMedium"] = "TrailerKI5utilityMedium_",
+}, {
+    SuspensionFrontLeft = {img="suspension_front_left", x=13,y=188,x2=55,y2=225},
+    SuspensionFrontRight = {img="suspension_front_right", x=228,y=188,x2=270,y2=225},
+    --
+    TireFrontLeft = {img="wheel_front_left", x=13,y=225,x2=55,y2=263},
+    TireFrontRight = {img="wheel_front_right", x=228,y=225,x2=270,y2=263},
+    --
+    TrunkDoor = {img="trunk", x=94,y=304,x2=187,y2=309},
+}, 10, 0);
+--
+--##########TrailerKI5utilitySmall##########
+--
+DAMN.MechOverlay:addParts({
+    ["Base.TrailerKI5utilitySmall"] = "TrailerKI5utilitySmall_",
+}, {
+    SuspensionFrontLeft = {img="suspension_front_left", x=13,y=188,x2=55,y2=225},
+    SuspensionFrontRight = {img="suspension_front_right", x=228,y=188,x2=270,y2=225},
+    --
+    TireFrontLeft = {img="wheel_front_left", x=13,y=225,x2=55,y2=263},
+    TireFrontRight = {img="wheel_front_right", x=228,y=225,x2=270,y2=263},
+    --
+    TrunkDoor = {img="trunk", x=94,y=289,x2=187,y2=294},
+}, 10, 0);
+--
+--##########TrailerKI5cargoLarge##########
+--
+DAMN.MechOverlay:addParts({
+    ["Base.TrailerKI5cargoLarge"] = "TrailerKI5cargoLarge_",
+    ["Base.TrailerKI5livestock"] = "TrailerKI5cargoLarge_",
+}, {
+    SuspensionFrontLeft = {img="suspension_front_left", x=13,y=188,x2=55,y2=225},
+    SuspensionFrontRight = {img="suspension_front_right", x=228,y=188,x2=270,y2=225},
+    SuspensionRearLeft = {img="suspension_rear_left", x=13,y=286,x2=55,y2=324},
+    SuspensionRearRight = {img="suspension_rear_right", x=228,y=286,x2=270,y2=324},
+    --
+    TireFrontLeft = {img="wheel_front_left", x=13,y=225,x2=55,y2=263},
+    TireFrontRight = {img="wheel_front_right", x=228,y=225,x2=270,y2=263},
+    TireRearLeft = {img="wheel_rear_left", x=13,y=324,x2=55,y2=361},
+    TireRearRight = {img="wheel_rear_right", x=228,y=324,x2=270,y2=361},
+    --
+    TrunkDoor = {img="trunk", x=94,y=375,x2=186,y2=380},
+}, 10, 0);
+--
+--##########TrailerKI5cargoMedium##########
+--
+DAMN.MechOverlay:addParts({
+    ["Base.TrailerKI5cargoMedium"] = "TrailerKI5cargoMedium_",
+}, {
+    SuspensionFrontLeft = {img="suspension_front_left", x=13,y=188,x2=55,y2=225},
+    SuspensionFrontRight = {img="suspension_front_right", x=228,y=188,x2=270,y2=225},
+    --
+    TireFrontLeft = {img="wheel_front_left", x=13,y=225,x2=55,y2=263},
+    TireFrontRight = {img="wheel_front_right", x=228,y=225,x2=270,y2=263},
+    --
+    TrunkDoor = {img="trunk", x=94,y=311,x2=186,y2=316},
+}, 10, 0);
+--
+--##########TrailerKI5cargoSmall##########
+--
+DAMN.MechOverlay:addParts({
+    ["Base.TrailerKI5cargoSmall"] = "TrailerKI5cargoSmall_",
+}, {
+    SuspensionFrontLeft = {img="suspension_front_left", x=13,y=188,x2=55,y2=225},
+    SuspensionFrontRight = {img="suspension_front_right", x=228,y=188,x2=270,y2=225},
+    --
+    TireFrontLeft = {img="wheel_front_left", x=13,y=225,x2=55,y2=263},
+    TireFrontRight = {img="wheel_front_right", x=228,y=225,x2=270,y2=263},
+    --
+    TrunkDoor = {img="trunk", x=94,y=286,x2=186,y2=291},
+}, 10, 0);
+--
---
+++ Contents/mods/KI5trailers/media/lua/shared/KI5trailers_SpawnList.lua
@@ -0,0 +1,38 @@
+if VehicleZoneDistribution then
+
+VehicleZoneDistribution.parkingstall.vehicles["Base.TrailerKI5utilityLarge"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.parkingstall.vehicles["Base.TrailerKI5utilityMedium"] = {index = -1, spawnChance = 2};
+VehicleZoneDistribution.parkingstall.vehicles["Base.TrailerKI5utilitySmall"] = {index = -1, spawnChance = 3};
+VehicleZoneDistribution.parkingstall.vehicles["Base.TrailerKI5cargoLarge"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.parkingstall.vehicles["Base.TrailerKI5cargoMedium"] = {index = -1, spawnChance = 2};
+VehicleZoneDistribution.parkingstall.vehicles["Base.TrailerKI5cargoSmall"] = {index = -1, spawnChance = 3};
+
+VehicleZoneDistribution.trailerpark.vehicles["Base.TrailerKI5utilityLarge"] = {index = -1, spawnChance = 2};
+VehicleZoneDistribution.trailerpark.vehicles["Base.TrailerKI5utilityMedium"] = {index = -1, spawnChance = 2};
+VehicleZoneDistribution.trailerpark.vehicles["Base.TrailerKI5utilitySmall"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.trailerpark.vehicles["Base.TrailerKI5cargoLarge"] = {index = -1, spawnChance = 2};
+VehicleZoneDistribution.trailerpark.vehicles["Base.TrailerKI5cargoMedium"] = {index = -1, spawnChance = 2};
+VehicleZoneDistribution.trailerpark.vehicles["Base.TrailerKI5cargoSmall"] = {index = -1, spawnChance = 1};
+
+VehicleZoneDistribution.bad.vehicles["Base.TrailerKI5utilityLarge"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.bad.vehicles["Base.TrailerKI5utilityMedium"] = {index = -1, spawnChance = 2};
+VehicleZoneDistribution.bad.vehicles["Base.TrailerKI5utilitySmall"] = {index = -1, spawnChance = 3};
+VehicleZoneDistribution.bad.vehicles["Base.TrailerKI5cargoLarge"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.bad.vehicles["Base.TrailerKI5cargoMedium"] = {index = -1, spawnChance = 2};
+VehicleZoneDistribution.bad.vehicles["Base.TrailerKI5cargoSmall"] = {index = -1, spawnChance = 3};
+
+VehicleZoneDistribution.junkyard.vehicles["Base.TrailerKI5utilityLarge"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.junkyard.vehicles["Base.TrailerKI5utilityMedium"] = {index = -1, spawnChance = 2};
+VehicleZoneDistribution.junkyard.vehicles["Base.TrailerKI5utilitySmall"] = {index = -1, spawnChance = 3};
+VehicleZoneDistribution.junkyard.vehicles["Base.TrailerKI5cargoLarge"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.junkyard.vehicles["Base.TrailerKI5cargoMedium"] = {index = -1, spawnChance = 2};
+VehicleZoneDistribution.junkyard.vehicles["Base.TrailerKI5cargoSmall"] = {index = -1, spawnChance = 3};
+
+VehicleZoneDistribution.trafficjams.vehicles["Base.TrailerKI5utilityLarge"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.trafficjams.vehicles["Base.TrailerKI5utilityMedium"] = {index = -1, spawnChance = 2};
+VehicleZoneDistribution.trafficjams.vehicles["Base.TrailerKI5utilitySmall"] = {index = -1, spawnChance = 3};
+VehicleZoneDistribution.trafficjams.vehicles["Base.TrailerKI5cargoLarge"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.trafficjams.vehicles["Base.TrailerKI5cargoMedium"] = {index = -1, spawnChance = 2};
+VehicleZoneDistribution.trafficjams.vehicles["Base.TrailerKI5cargoSmall"] = {index = -1, spawnChance = 3};
+
+end
